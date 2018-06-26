5e4776825fbde85ee7e1d6e8e983020d3c83be2f
martysun25 <martysun25@gmail.com>
2018-06-27 02:12:14 +0800

    qweqweqwe

--- App.tsx
+++ App.tsx
@@ -2,25 +2,22 @@ import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export default class App extends Component<{ a: string }, {}> {
-render() {
-const a = this.props.a;
-return (
-<View style={styles.container}>
-<Text>Open up App.ts to start working on your app!</Text>
-<Text>Changes you make will automatically reload.</Text>
-</View>
-);
-
-
-
-}
+  render() {
+    const a = this.props.a;
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.ts to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-container: {
-alignItems: "center",
-backgroundColor: "#fff",
-flex: 1,
-justifyContent: "center",
-},
+  container: {
+    alignItems: "center",
+    backgroundColor: "#fff",
+    flex: 1,
+    justifyContent: "center",
+  },
 });
